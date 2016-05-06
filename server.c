7388e83587d74aff8942a9d1076d3758fc62a774
Mariusz <19mariusz95@gmail.com>
2016-05-06 22:35:26 +0200

    .

--- server.c
+++ server.c
@@ -183,7 +183,6 @@ int get_free_id() {
 
 BOOL CtrlHandler(DWORD fdwCtrlType) {
     switch (fdwCtrlType) {
-        // Handle the CTRL-C signal.
         case CTRL_C_EVENT:
             printf("Ctrl-C event\n\n");
             Beep(750, 300);
@@ -216,41 +215,24 @@ int main(int argc, char *argv[]) {
         printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
-
     printf("Initialised.\n");
-
     if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
         printf("Could not create socket : %d", WSAGetLastError());
     }
-
     printf("Socket created.\n");
-
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8888);
-
     if (bind(s, (struct sockaddr *) &server, sizeof(server)) == SOCKET_ERROR) {
         printf("Bind failed with error code : %d", WSAGetLastError());
     }
-
     puts("Bind done");
-
-
     listen(s, 3);
-
     puts("Waiting for incoming connections...");
-
     c = sizeof(struct sockaddr_in);
-
     while ((new_socket = accept(s, (struct sockaddr *) &client, &c)) != INVALID_SOCKET) {
         puts("Connection accepted");
         CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) ThreadFunc, &new_socket, 0, NULL);
     }
-
-    if (new_socket == INVALID_SOCKET) {
-        printf("accept failed with error code : %d", WSAGetLastError());
-        return 1;
-    }
-
     return 0;
 }
