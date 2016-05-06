215e6e19ca912d6cdaa62fc4145e6b846fc66766
Mariusz <19mariusz95@gmail.com>
2016-05-06 19:51:59 +0200

    refactor

--- client.c
+++ client.c
@@ -8,18 +8,26 @@
 #include<winsock2.h>
 #include "main.h"
 
-void draw_board(char string[2000]);
+SOCKET s;
+WSADATA wsa;
+struct sockaddr_in server;
+
+void draw_board();
+
+void init();
 
 int main(int argc, char *argv[]) {
 
-    WSADATA wsa;
-    SOCKET s;
-    struct sockaddr_in server;
+    init();
+    draw_board();
+    return 0;
+}
 
+void init() {
     printf("\nInitialising Winsock...");
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Failed. Error Code : %d", WSAGetLastError());
-        return 1;
+        exit(1);
     }
 
     printf("Initialised.\n");
@@ -39,36 +47,33 @@ int main(int argc, char *argv[]) {
     //Connect to remote server
     if (connect(s, (struct sockaddr *) &server, sizeof(server)) < 0) {
         puts("connect error");
-        return 1;
+        exit(1);
     }
 
     puts("Connected");
+}
+
+void draw_board() {
 
     char message[10];
     message[0] = GETBOARD;
     if (send(s, message, strlen(message), 0) < 0) {
         puts("Send failed");
-        return 1;
+        return;
     }
 
     //Receive a reply from the server
     int recv_size;
-    char server_reply[2000];
-    if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR) {
+    char string[2000];
+    if ((recv_size = recv(s, string, 2000, 0)) == SOCKET_ERROR) {
         puts("recv failed");
     }
 
     puts("Reply received\n");
 
     //Add a NULL terminating character to make it a proper string before printing
-    server_reply[recv_size] = '\0';
+    string[recv_size] = '\0';
 
-    draw_board(server_reply);
-
-    return 0;
-}
-
-void draw_board(char string[2000]) {
     for (int k = 0; k < 2; k++) {
         printf("   ");
         for (int i = 0; i < 10; i++) {
